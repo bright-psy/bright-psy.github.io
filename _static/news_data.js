5999e54154b535f0736289b6ce7717072af1c409
Pierre Bergeret <pierre.bergeret12@gmail.com>
2025-10-29 17:51:23 -0400

    correct leanael

--- _static/news_data.js
+++ _static/news_data.js
@@ -4,10 +4,7 @@ const NEWS_DATA = [
     date: "2025-10-24",
     title: "Two visiting PhD students join the lab!",
     description: `We are excited to welcome Anaël Ayrolles from Robert-Debré University Hospital and Léa Waller from Charité - Universitätsmedizin Berlin to our team as visiting PhD students for 3 months. Welcome aboard!`,
-    image: [
-      "_static/images/aayrolles.jpeg",
-      "_static/images/lwaller.jpg"
-    ]
+    image: "_static/images/leanael.png"
   },  
   {
     topic: "projects",
